45ee6603bbac196c50e3c2c0444efb47cf4be70b
elizabethjmohr <elizabethjmohr@gmail.com>
2022-08-15 08:16:54 -0500

    edit HOP

--- HOP.R
+++ HOP.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(read4cast)
 library(lubridate)
 library(cowplot)
 library(gganimate)
@@ -6,55 +7,21 @@ library(magick)
 library(av)
 
 # read EFI null temperature forecast for the aquatics challenge
-aquatics_null <- read4cast::read_forecast(paste0("https://data.ecoforecast.org/forecasts/aquatics/",
+null_forecast <- read4cast::read_forecast(paste0("https://data.ecoforecast.org/forecasts/aquatics/",
                                       "aquatics-2020-09-01-EFInull.csv.gz")) %>%
   mutate(time = as_date(time)) %>%
   filter(variable == "temperature",
          site_id == "BARC")
 
-# calculate median and 95% prediction interval bounds for each day
-summary <- aquatics_null %>%
-  group_by(time) %>%
-  summarise(median = median(predicted, na.rm = TRUE),
-            lower = quantile(predicted, 0.025),
-            upper = quantile(predicted, 0.975))
+observed <- neon4cast::combined_scores("aquatics") |>
+  filter(time >= lubridate::as_datetime('2020-09-01'), 
+         time < lubridate::as_datetime('2020-09-08'), 
+         variable == "temperature", 
+         !is.na(observed)) |>
+  group_by(time) |>
+  summarise(observed = median(observed))
 
-set.seed(8953762)
-n_frames <- 100 # Total number of ensembles in animation
-random_ensembles <- sample(unique(aquatics_null$ensemble),
-                           size = n_frames)
-
-HOP1 <- aquatics_null %>% 
-  filter(ensemble %in% random_ensembles) %>%
-  mutate(ensemble_rank = dense_rank(ensemble)) %>%
-  ggplot()+
-  geom_ribbon(data = summary, 
-              aes(x = time, ymin = lower, ymax = upper), 
-              fill = "#588B8E",
-              alpha = 0.3)+
-  geom_line(data = summary, aes(x = time, y = median), 
-            color = "#588B8E", size = 1.5)+
-  geom_point(aes(x = time, y = predicted))+
-  geom_line(aes(x = time, y = predicted, group = ensemble))+
-  scale_x_date(date_breaks = "1 day",
-               date_labels = "%b-%d") +
-  ylab("predicted temperature (degrees Celsius)")+
-  theme_minimal_grid()+
-  theme(axis.title.x = element_blank(),
-        legend.position = 'none')+
-  transition_manual(ensemble_rank)
-
-animation1 <- animate(HOP1, fps = 2.5)
-anim_save("./HOP1.gif", 
-          animation = animation1)
-
-gif1 <- image_read("./HOP1.gif")
-image_write_video(gif1,
-                  path = "./HOP1.mp4",
-                  framerate = 2.5)
-
-
-HOP2 <- aquatics_null %>% 
+HOP <- null_forecast %>% 
   filter(ensemble %in% random_ensembles) %>%
   mutate(ensemble_rank = dense_rank(ensemble)) %>%
   uncount(n_frames, .id="frame") %>%
@@ -63,28 +30,38 @@ HOP2 <- aquatics_null %>%
   group_by(frame) %>%
   mutate(alpha = if_else(ensemble_rank == frame, 1, 0.5)) %>%
   ungroup() %>%
-  mutate(datetime = as_datetime(paste(time, "00:00:00")),
-         xmin = datetime - hours(9), 
-         xmax = datetime + hours(9))%>%
-  ggplot()+
-  geom_segment(aes(x = xmin, xend = xmax, y = predicted, yend = predicted, alpha = alpha),
-               size = 2,
-               color = "#00bfff")+
-  transition_manual(frame)+
-  scale_x_datetime(date_breaks = "1 day",
-               date_labels = "%b-%d") +
-  ylab("predicted temperature (degrees Celsius)")+
-  theme_minimal_hgrid()+
+  mutate(
+    datetime = as_datetime(paste(time, "00:00:00")),
+    xmin = datetime - hours(9), 
+    xmax = datetime + hours(9)
+  ) %>%
+  ggplot() +
+  geom_segment(
+    aes(x = xmin, xend = xmax, y = predicted, yend = predicted, 
+        alpha = alpha, color = 'predicted'),
+    size = 2) +
+  transition_manual(frame) +
+  geom_point(data = observed, aes(x = time, y = observed, color = 'observed')) +
+  scale_color_manual(
+    name = NULL,
+    values = c('observed'  = "#000000", 'predicted' = '#00bfff'),
+    labels = c('observed', 'predicted'), 
+    guide = guide_legend(override.aes = list(linetype = c(0,1), shape = c(16,NA)))) +
+  scale_x_datetime(date_breaks = "1 day", date_labels = "%b-%d") +
+  ylab("temperature (degrees Celsius)") +
+  theme_minimal_hgrid() +
+  scale_alpha(guide = 'none') + 
   theme(axis.title.x = element_blank(),
-        legend.position = 'none')
+        legend.position = 'right') + 
+  ggtitle('Number of ensembles: {frame}')
 
-animation2 <- animate(HOP2, fps = 2.5)
-anim_save("./HOP2.gif", 
-          animation = animation2)
+animation <- animate(HOP, fps = 2.5)
+anim_save("./HOP.gif", 
+          animation = animation)
 
-gif2 <- image_read("./HOP2.gif")
-image_write_video(gif2,
-                  path = "./HOP2.mp4",
+gif <- image_read("./HOP.gif")
+image_write_video(gif,
+                  path = "./HOP.mp4",
                   framerate = 2.5)
 
 
